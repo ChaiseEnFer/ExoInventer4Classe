6cfb5fa8c58cb26f91c6d7f117ee1fa889c29fc0
ChaiseEnFer <maxime.brunel.lycee@gmail.com>
2024-11-04 16:58:49 +0100

    class Membres

---
+++ ConsoleApp1/Membres.cs
@@ -0,0 +1,26 @@
+﻿//membres
+//livres
+//emprunts
+//bibliothèque
+namespace Program
+{
+    public class Membres
+    {
+        string Nom;
+        string Prenom;
+        DateOnly DateNaissance;
+
+        public Membres(string nom, string prenom, DateOnly dateNaissance)
+        {
+            Nom = nom;
+            Prenom = prenom;
+            DateNaissance = dateNaissance;
+        }
+
+        public override string ToString()
+        {
+            string toString = String.Format($"Le membre se nomm {this.Prenom} {this.Nom}\nil est née :{this.DateNaissance}");
+            return toString;
+        }
+    }
+}
